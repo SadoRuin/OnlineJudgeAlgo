66bc1351d5cae9557821cd5427af578817730c13
SadoRuin <47595515+SadoRuin@users.noreply.github.com>
2022-08-03 21:43:56 +0900

    [level 2] Title: 올바른 괄호, Time: 17.80 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,23 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = false;
-        int cnt = 0;
+        boolean answer = true;
+        Deque<Character> dq = new ArrayDeque<>();
         for(int i=0; i<s.length(); i++) {
             if(s.charAt(i) == '(') {
-                cnt++;
+                dq.offer(s.charAt(i));
             } else if(s.charAt(i) == ')') {
-                cnt--;
-                if(cnt < 0) {
+                if(dq.isEmpty()) {
+                    answer = false;
                     break;
+                } else {
+                    dq.poll();
                 }
             }
         }
-        
-        if(cnt == 0) {
-            answer = true;
+        if(!dq.isEmpty()) {
+            answer = false;
         }
 
         return answer;
