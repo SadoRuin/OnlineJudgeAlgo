c4dbbfd1b784a7c633ce6a77383627d875dae760
SadoRuin <47595515+SadoRuin@users.noreply.github.com>
2022-08-04 23:30:26 +0900

    [level 2] Title: 올바른 괄호, Time: 17.50 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        Deque<Character> dq = new ArrayDeque<>();
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '(') {
-                dq.offer(s.charAt(i));
-            } else if(s.charAt(i) == ')') {
-                if(dq.isEmpty()) {
-                    answer = false;
-                    break;
-                } else {
-                    dq.poll();
+        boolean answer = true;  // 기본 반환값을 true로
+        Deque<Character> dq = new ArrayDeque<>();   // ArrayDeque를 Stack으로 사용
+        for(int i=0; i<s.length(); i++) {   // 주어진 괄호 문자열을 탐색
+            if(s.charAt(i) == '(') {    // 여는 괄호라면
+                dq.offer(s.charAt(i));  // stack에 저장
+            } else if(s.charAt(i) == ')') { // 닫는 괄호라면
+                if(dq.isEmpty()) {  // stack이 비어 있다면 잘못된 괄호 문자열이므로
+                    answer = false; // 반환값에 false를 저장하고
+                    break;          // break
+                } else {            // 비어있지 않다면
+                    dq.pollLast();  // stack의 top을 꺼낸다
                 }
             }
         }
-        if(!dq.isEmpty()) {
-            answer = false;
+        if(!dq.isEmpty()) { // 모든 괄호를 탐색하고도 스택에 괄호가 남아있다면 잘못된 괄호 문자열이므로
+            answer = false; // 반환값에 false를 저장
         }
 
         return answer;
