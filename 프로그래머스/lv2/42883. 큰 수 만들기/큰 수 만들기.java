4bdbfdf5f62e12fae2b7686cdc509bf29ec835d4
Park YunHwan <47595515+SadoRuin@users.noreply.github.com>
2022-08-14 01:44:26 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.07 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,25 +1,29 @@
+import java.util.ArrayDeque;
+
 class Solution {
     public String solution(String number, int k) {
         int N = number.length();    // 주어진 숫자의 자리수
         int R = N - k;      // 남겨야 하는 자리수
         StringBuilder answer = new StringBuilder(); // 선택된 숫자를 저장할 StringBuilder
+        ArrayDeque<Integer> dq = new ArrayDeque<>();    // 선택 숫자를 저장할 스택
 
-        int index = 0;
-        int bound = N - R;
-        // 앞의 자리수가 클수록 큰 수가 만들어지므로
-        // 만들 수 있는 자리수가 되면서 가장 앞자리수의 최대값을 결정할 수 있는 최대 범위가 bound이다
-        while(answer.length() != R) {     // 선택된 숫자가 남겨야하는 자리수만큼 선택될때까지
-            int max = 0;
-            for(int i=index; i<=bound; i++) {    // 특정 index부터 bound까지
-                if(i >= N) break;   // 탐색 범위가 주어진 숫자 길이를 벗어나면 break
-                int num = number.charAt(i) - '0';
-                if(max < num) {     // 큰 숫자가 있으면
-                    max = num;  // 큰 숫자를 저장하고
-                    index = i+1;    // 그 큰 순자의 다음 자리를 새로운 index로
+        for(int i=0; i<N; i++) {    // 숫자 탐색
+            int num = number.charAt(i) - '0';
+            while(!dq.isEmpty() && k>0) {   // 스택이 비어있지 않고 k가 0보다 크면 반복
+                int top = dq.peek();
+                if(top < num) {     // stack의 top이 이번 숫자보다 작으면
+                    dq.pop();   // stack을 pop하고
+                    k--;    // k는 1 감소
+                } else {    // top이 더이상 작지 않으면 break;
+                    break;
                 }
             }
-            answer.append(max);    // 범위에서 정해진 최종 큰 숫자를 선택하고
-            bound++;    // bound를 하나 늘린다
+            dq.push(num);   // 현재 숫자를 stack에 push
+        }
+        while(k-- > 0) dq.pop();  // 숫자를 전부 탐색하고도 k가 남았다면 그 수만큼 pop
+
+        while(!dq.isEmpty()) {
+            answer.append(dq.pollLast());   // 남은 stack의 숫자를 stack의 반대순서로 정렬
         }
 
         return answer.toString();
