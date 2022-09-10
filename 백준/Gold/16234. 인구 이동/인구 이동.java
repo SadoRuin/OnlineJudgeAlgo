e56c0d459ea5fe78a63dae0e91204d52691e243f
Park YunHwan <47595515+SadoRuin@users.noreply.github.com>
2022-09-10 17:21:02 +0900

    [Gold V] Title: 인구 이동, Time: 488 ms, Memory: 114092 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -14,51 +14,52 @@ public class Main {
     static boolean[][] visited;
     static List<int[]> list;
 
+    // dfs 메소드
     static void dfs(int x, int y) {
-
-        for(int i=0; i<4; i++) {
+        for(int i=0; i<4; i++) {    // 우, 하, 좌, 상 반복
             int nextX = x + dx[i];
             int nextY = y + dy[i];
-            if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && !visited[nextX][nextY]) {
-                int diff = Math.abs(map[x][y] - map[nextX][nextY]);
-                if(diff >= L && diff <= R) {
-                    visited[nextX][nextY] = true;
-                    list.add(new int[] {nextX, nextY});
-                    sum += map[nextX][nextY];
-                    dfs(nextX, nextY);
+            if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && !visited[nextX][nextY]) {  // 다음위치가 범위 내이면서
+                int diff = Math.abs(map[x][y] - map[nextX][nextY]); // 다음 위치와의 인구 차이
+                if(diff >= L && diff <= R) {    // 인구차이가 L이상 R이하이면
+                    visited[nextX][nextY] = true;   // 방문처리
+                    list.add(new int[] {nextX, nextY}); // 다음 위치를 리스트에 추가
+                    sum += map[nextX][nextY];   // 그룹 인구수 합에 더함
+                    dfs(nextX, nextY);  // 다음 위치 dfs
                 }
             }
         }
     }
 
-    static int move() {
+    // 인구이동 메소드
+    static boolean move() {
         for (boolean[] v :
                 visited) {
-            Arrays.fill(v, false);
+            Arrays.fill(v, false);  // 방문기록 초기화
         }
-        int cnt = 0;
+        boolean isMove = false;    // 인구이동이 있었는지 확인
         for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                if (!visited[i][j]) {
-                    sum = map[i][j];
-                    visited[i][j] = true;
-                    list.add(new int[] {i, j});
-                    dfs(i, j);
-                    int size = list.size();
-                    if(size > 1) {
+            for(int j=0; j<N; j++) {    // 모든 지역 확인
+                if (!visited[i][j]) {   // 방문하지 않았으면
+                    sum = map[i][j];    // 그룹 합을 현재 인구수로 초기화
+                    visited[i][j] = true;   // 현재 지역 방문처리
+                    list.add(new int[] {i, j}); // 현재 지역 좌표 리스트에 저장
+                    dfs(i, j);  // dfs 시작
+                    int size = list.size(); // 그룹 좌표 리스트 크기
+                    if(size > 1) {  // 리스트의 내용이 시작지역 1개보다 많다면
                         for (int[] p :
-                                list) {
+                                list) { // 리스트 좌표 반복
                             int x = p[0];
                             int y = p[1];
-                            map[x][y] = sum / size;
+                            map[x][y] = sum / size; // 그룹에 속하는 지역의 인구수를 모두 똑같게
                         }
-                        cnt++;
+                        isMove = true;  // 인구이동이 있었으므로 인구이동 여부를 true로
                     }
-                    list.clear();
+                    list.clear();   // 그룹 리스트 초기화
                 }
             }
         }
-        return cnt;
+        return isMove;  // 인구이동 여부 반환
     }
 
     public static void main(String[] args) throws Exception {
@@ -79,10 +80,9 @@ public class Main {
             }
         }
 
-        sum = 0;
         list = new ArrayList<>();
-        while(move() > 0) {
-            totalCnt++;
+        while(move()) { // 인구이동이 있는 동안 반복
+            totalCnt++; // 인구이동 카운트 증가
         }
 
         System.out.println(totalCnt);
