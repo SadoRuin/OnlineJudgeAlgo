ae8fcdf5abfc2ec88b34e4ff968e32bc8338c2b3
Park YunHwan <47595515+SadoRuin@users.noreply.github.com>
2022-10-06 14:45:16 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 125 ms, Memory: 30,492 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 class Solution {
     public static void main(String[] args) throws Exception {
@@ -16,20 +17,20 @@ class Solution {
             for(int i=0; i<N; i++) {
                 A[i] = Integer.parseInt(st[i]);
             }
+			int[] C = new int[N];   // 동적테이블 C[k] : 해당(k) 길이를 만족하는 자리(k자리)에 오는 수의 최소값
+            int size = 0;
 
-            int[] B = new int[N];   // 동적테이블 : 각 원소를 끝으로하는 LIS값
-            int max = 0;
-            for(int i=0; i<N; i++) {    // 앞쪽부터 모든 원소기준으로 자신을 끝으로하는 LIS 계산
-                B[i] = 1;
-                for(int j=0; j<i; j++) {
-                    if(A[j] < A[i] && B[i] < B[j] + 1) {
-                        B[i] = B[j] + 1;
-                    }
-                }
-                max = Math.max(max, B[i]);
+            for(int i=0; i<N; i++) {
+                int pos = Arrays.binarySearch(C, 0, size, A[i]);
+                if(pos >= 0) continue;
+
+                int insertPos = Math.abs(pos) - 1;  // 맨뒤 또는 기존원소 대체자리
+                C[insertPos] = A[i];
+
+                if(insertPos == size) size++;
             }
 
-            sb.append("#").append(tc).append(" ").append(max).append("\n");
+            sb.append("#").append(tc).append(" ").append(size).append("\n");
         }
 
         System.out.println(sb);
