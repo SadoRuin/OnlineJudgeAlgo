0cbb6bc454a4b89b54170dca0d2a27e0219c51ed
Park YunHwan <47595515+SadoRuin@users.noreply.github.com>
2023-01-04 00:12:06 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 224 ms, Memory: 125716 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,35 +1,30 @@
 import sys
-from itertools import permutations
+from collections import deque
 
 input = sys.stdin.readline
 n = int(input())
 a = list(map(int, input().split()))
-exp = [['+'], ['-'], ['*'], ['/']]
-op = []
-for i, m in enumerate(input().split()):
-    for _ in range(int(m)):
-        op += exp[i]
+op = list(map(int, input().split()))
 
 mxm = -1e9
 mnm = 1e9
 
-for perm in permutations(op):
-    answer = a[0]
-    for i, p in enumerate(perm):
-        if p == '+':
-            answer += a[i+1]
-        elif p == '-':
-            answer -= a[i+1]
-        elif p == '*':
-            answer *= a[i+1]
-        elif p == '/':
-            if answer < 0:
-                answer = ((answer * -1) // a[i+1]) * -1
-            else:
-                answer = answer // a[i+1]
-    mxm = max(mxm, answer)
-    mnm = min(mnm, answer)
+queue = deque([[a[0], op[0], op[1], op[2], op[3], 1]])
 
+while queue:
+    num, add, sub, mul, div, cnt = queue.popleft()
+    if cnt == n:
+        mxm = max(mxm, num)
+        mnm = min(mnm, num)
+
+    if add > 0:
+        queue.append([num + a[cnt], add-1, sub, mul, div, cnt+1])
+    if sub > 0:
+        queue.append([num - a[cnt], add, sub-1, mul, div, cnt+1])
+    if mul > 0:
+        queue.append([num * a[cnt], add, sub, mul-1, div, cnt+1])
+    if div > 0:
+        queue.append([int(num / a[cnt]), add, sub, mul, div-1, cnt+1])
 
 print(mxm)
 print(mnm)
